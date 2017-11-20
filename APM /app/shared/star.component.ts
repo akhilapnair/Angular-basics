854f7f576be2cafec159e2cfa6b08a598843c735
akhilapnair <akhi.prabha@gmail.com>
2017-11-20 23:16:38 +0530

    start component decorator

--- APM /app/shared/star.component.ts
+++ APM /app/shared/star.component.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
+@Component( {
+    selector: 'ai-star',
+    templateUrl: 'app/shared/star.component.html',
+    styleUrls: ['app/shared/star.component.css']
+})
 export class StarComponent {
 	
 }
