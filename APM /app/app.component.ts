e80d6a4afdb77bf6415ec5c1fdc2b0f4f770e7e7
akhila <akhi.prabha@gmail.com>
2017-12-04 14:43:48 +0530

    UI fixes in home page

--- APM /app/app.component.ts
+++ APM /app/app.component.ts
@@ -3,11 +3,15 @@ import { ProductService } from './products/product.service';
 @Component({
     selector: 'pm-app',
     template: `
+    <nav class='navbar navbar-default'>
+    <div class='container-fluid'>
     <a class='navbar-brand'>{{pageTitle}}</a>
     <ul class='nav navbar-nav'>
     <li><a [routerLink]="['/welcome']" routerLinkActive="active">Home</a></li>
     <li><a [routerLink]="['/product']" routerLinkActive="active">Product List</a></li>
     </ul>
+    </div>
+    </nav>
     <div class = 'container'>
     <router-outlet></router-outlet>
     </div>
